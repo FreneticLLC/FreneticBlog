6780efe4192608b74dad28f60d69b5365b58ae9b
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-05-21 12:04:29 -0700

    add an autobuilder script

---
+++ autobuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git fetch origin
+changed_files="$(git diff-tree -r --name-only --no-commit-id origin/master HEAD)"
+
+if [ -n "$changed_files" ]
+then
+    echo "Changes: $changed_files at $(date)" >> ~/checklogs.log
+    git pull origin master
+    rm -rf _site/
+    jekyll build
+fi
+
