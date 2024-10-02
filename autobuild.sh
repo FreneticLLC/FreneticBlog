1cc18b60be401c807f73245600a1ac38a31b88fe
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-10-02 06:26:36 -0700

    additional fix

--- autobuild.sh
+++ autobuild.sh
@@ -6,6 +6,7 @@ changed_files="$(git diff-tree -r --name-only --no-commit-id origin/master HEAD)
 if [ -n "$changed_files" ]
 then
     echo "Changes: $changed_files at $(date)" >> ~/checklogs.log
+    git add .
     git stash
     git pull origin master
     rm -rf _site/
